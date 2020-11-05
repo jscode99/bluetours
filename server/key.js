0681dcc3ba3a98a5983f15c709eceec80d5d67bf
Jishnu Satheesh <34735680+jscode99@users.noreply.github.com>
2020-11-05 20:18:58 +0530

    deleted keys
    accidentally pushed keys

--- server/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGOURI:
-        "mongodb+srv://jishnu:admin@cluster0.si4g4.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  PORT: 4000,
-    JWT_SECRET:"LOVEYOU3000"
-};
